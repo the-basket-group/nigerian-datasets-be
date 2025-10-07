692fa7764a96613f5ad91ef9e053984789dc8ad9
KeneNwogu <nwogujoseph1111@gmail.com>
2025-10-07 08:48:48 +0100

    fix: formatting fix

--- backend/settings.py
+++ backend/settings.py
@@ -159,7 +159,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
-    "https://ng-datasets-fe-864721386499.us-central1.run.app"
+    "https://ng-datasets-fe-864721386499.us-central1.run.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
