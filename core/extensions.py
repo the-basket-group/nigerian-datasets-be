03f9f6974f7da42205ee392b867c1cef3906eba9
KeneNwogu <59177747+KeneNwogu@users.noreply.github.com>
2025-10-08 12:29:32 +0100

    fix: routine fixes for docs along with atomic transaction for createdatasetview (#18)
    fix: drf-spectacular doc fixes

---
+++ core/extensions.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+from drf_spectacular.extensions import OpenApiAuthenticationExtension
+
+
+class JWTAuthenticationScheme(OpenApiAuthenticationExtension):
+    target_class = "users.auth.JWTAuthentication"
+    name = "JWTAuthentication"
+
+    def get_security_definition(self, auto_schema: Any) -> dict:
+        return {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+            "description": 'JWT Authorization header using the Bearer scheme. Example: "Authorization: Bearer ${token}"',
+        }
