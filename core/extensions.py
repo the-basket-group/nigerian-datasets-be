dc2ccdb6cf2f4e1b4fcbe899e65f5fa1251d4134
KeneNwogu <nwogujoseph1111@gmail.com>
2025-10-08 12:19:18 +0100

    fix: extension fix for example message

--- core/extensions.py
+++ core/extensions.py
@@ -12,5 +12,5 @@ class JWTAuthenticationScheme(OpenApiAuthenticationExtension):
             "type": "http",
             "scheme": "bearer",
             "bearerFormat": "JWT",
-            "description": 'JWT Authorization header using the Bearer scheme. Example: "Authorization: Bearer token"',
+            "description": 'JWT Authorization header using the Bearer scheme. Example: "Authorization: Bearer ${token}"',
         }
