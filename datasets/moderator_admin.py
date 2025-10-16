80c8b752b50ff6f6ec7987dabe6660ae5fef876c
Ifihanagbara Olusheye <victoriaolusheye@gmail.com>
2025-10-16 16:51:40 +0100

    feat: implemented the moderator side of things (#20)
    * feat: implemented the moderator side of things
    
    * feat: made changes

--- datasets/moderator_admin.py
+++ datasets/moderator_admin.py
@@ -1,13 +1,32 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from django.db.models import QuerySet
 from django.http import HttpRequest
 from django.utils.html import format_html
 
-from datasets.models import Dataset, DatasetFile, DatasetVersion, Tag
+from datasets.models import Dataset
 
 
-@admin.register(Dataset)
-class DatasetAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+class ModeratorAdminSite(AdminSite):
+    site_header = "Moderator Dashboard"
+    site_title = "Dataset Moderation"
+    index_title = "Approve Datasets"
+
+    def has_permission(self, request: HttpRequest) -> bool:
+        """Allow moderators only"""
+        return (
+            request.user.is_active
+            and request.user.is_staff
+            and request.user.role == "moderator"
+        )
+
+
+# Create the moderator admin site instance
+moderator_admin_site = ModeratorAdminSite(name="moderator_admin")
+
+
+@admin.register(Dataset, site=moderator_admin_site)
+class ModeratorDatasetAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
     list_display = [
         "title",
         "owner",
@@ -20,7 +39,26 @@ class DatasetAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
     ]
     list_filter = ["is_approved", "status", "is_public", "created_at"]
     search_fields = ["title", "description", "owner__email"]
-    readonly_fields = ["id", "views", "downloads", "created_at", "updated_at"]
+    readonly_fields = [
+        "id",
+        "title",
+        "description",
+        "license",
+        "source_org",
+        "geography",
+        "update_frequency",
+        "status",
+        "is_public",
+        "metadata",
+        "owner",
+        "tags",
+        "completeness_score",
+        "changelog",
+        "views",
+        "downloads",
+        "created_at",
+        "updated_at",
+    ]
     actions = ["approve_datasets", "reject_datasets"]
 
     def approval_status(self, obj: Dataset) -> str:
@@ -51,54 +89,14 @@ class DatasetAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
     def get_queryset(self, request: HttpRequest) -> QuerySet[Dataset]:
         qs = super().get_queryset(request)
         # Moderators only see datasets pending approval
-        if hasattr(request.user, "role") and request.user.role == "moderator":
-            return qs.filter(is_approved=False)
-        return qs
+        return qs.filter(is_approved=False)
 
     def has_add_permission(self, request: HttpRequest) -> bool:
-        # Moderators cannot create datasets via admin
-        if hasattr(request.user, "role") and request.user.role == "moderator":
-            return False
-        return super().has_add_permission(request)
+        # Moderators cannot create datasets
+        return False
 
     def has_delete_permission(
         self, request: HttpRequest, obj: Dataset | None = None
     ) -> bool:
         # Moderators cannot delete datasets
-        if hasattr(request.user, "role") and request.user.role == "moderator":
-            return False
-        return super().has_delete_permission(request, obj)
-
-    def has_change_permission(
-        self, request: HttpRequest, obj: Dataset | None = None
-    ) -> bool:
-        # Moderators can only change approval status
-        return True
-
-    def get_readonly_fields(
-        self, request: HttpRequest, obj: Dataset | None = None
-    ) -> list[str]:
-        readonly = list(self.readonly_fields)
-        # Moderators can only change approval fields
-        if hasattr(request.user, "role") and request.user.role == "moderator":
-            readonly.extend(
-                [
-                    "title",
-                    "description",
-                    "license",
-                    "source_org",
-                    "geography",
-                    "update_frequency",
-                    "status",
-                    "is_public",
-                    "metadata",
-                    "owner",
-                    "tags",
-                    "completeness_score",
-                    "changelog",
-                ]
-            )
-        return readonly
-
-
-admin.site.register([DatasetVersion, DatasetFile, Tag])
+        return False
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,46 @@
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest
 
 from users.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    list_display = ["email", "first_name", "last_name", "role", "status", "is_staff"]
+    list_filter = ["role", "status", "is_staff"]
+    search_fields = ["email", "first_name", "last_name"]
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[User]:
+        qs = super().get_queryset(request)
+        # Moderators can only see themselves
+        if hasattr(request.user, "role") and request.user.role == "moderator":
+            return qs.filter(id=request.user.id)
+        return qs
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        # Only superusers can create users
+        if not request.user.is_superuser:
+            return False
+        return super().has_add_permission(request)
+
+    def has_change_permission(
+        self, request: HttpRequest, obj: User | None = None
+    ) -> bool:
+        # Moderators can only edit themselves
+        if hasattr(request.user, "role") and request.user.role == "moderator":
+            if obj is not None and obj.id != request.user.id:
+                return False
+            return True
+        # Only superusers can edit other users
+        if not request.user.is_superuser:
+            return False
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(
+        self, request: HttpRequest, obj: User | None = None
+    ) -> bool:
+        # Only superusers can delete users
+        if not request.user.is_superuser:
+            return False
+        return super().has_delete_permission(request, obj)
